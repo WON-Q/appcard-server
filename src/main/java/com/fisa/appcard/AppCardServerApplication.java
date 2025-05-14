0e3f0de05be70641fc4707cfc9a7825659efb538
namsh1125 <namsh1125@naver.com>
2025-05-14 09:20:52 +0900

    Init: 프로젝트 초기 설정

---
+++ src/main/java/com/fisa/appcard/AppCardServerApplication.java
@@ -0,0 +1,13 @@
+package com.fisa.appcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppCardServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppCardServerApplication.class, args);
+    }
+
+}
